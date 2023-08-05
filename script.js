5b93e87315b030b1c71a4f62a905d72cd51e2236
Nihalmullungal <nihalmullungal@gmail.com>
2023-08-05 15:53:44 +0530

    fourth

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 document.addEventListener("DOMContentLoaded", function (){
-const nav_br=document.getElementById('nv-btn')
-const hide=document.getElementById('shw')
-nav_br.addEventListener('click',function(){
-    const style=window.getComputedStyle(hide).display
-    alert('hi')
-    if(style=='block')
-    {
-        hide.style.display="none"
-        alert('hi')
-    }
-})
+
 })
